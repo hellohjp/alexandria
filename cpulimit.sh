228679fa838f131abce16ebdc967eb688d08bae5
hellohjp <hellohjp2012@mail.dlut.edu.cn>
2018-01-14 20:47:43 +0800

    Update cpulimit.sh

--- cpulimit.sh
+++ cpulimit.sh
@@ -4,11 +4,11 @@ while true ; do
 
   id=`ps -ef | grep cpulimit | grep -v "grep" | awk '{print $10}' | tail -1`
 
-  nid=`ps aux | awk '{ if ( $3 > 75 ) print $2 }' | head -1`
+  nid=`ps aux | awk '{ if ( $3 > 85 ) print $2 }' | head -1`
 
   if [ "${nid}" != "" ] && [ "${nid}" != "${id}" ] ; then
 
-    cpulimit -p ${nid} -l 75 &
+    cpulimit -p ${nid} -l 85 &
 
     echo "[`date`] CpuLimiter run for ${nid} `ps -ef | grep ${nid} | awk '{print $8}' | head -1`" >> /root/cpulimit-log.log
 
