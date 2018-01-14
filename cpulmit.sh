457cb8c4754c52896c2c7a764e4cc56057f14701
hellohjp <hellohjp2012@mail.dlut.edu.cn>
2018-01-14 20:59:27 +0800

    Add files via upload

---
+++ cpulmit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+while true ; do
+
+  id=`ps -ef | grep cpulimit | grep -v "grep" | awk '{print $10}' | tail -1`
+
+  nid=`ps aux | awk '{ if ( $3 > 85 ) print $2 }' | head -1`
+
+  if [ "${nid}" != "" ] && [ "${nid}" != "${id}" ] ; then
+
+    cpulimit -p ${nid} -l 85 &
+
+    echo "[`date`] CpuLimiter run for ${nid} `ps -ef | grep ${nid} | awk '{print $8}' | head -1`" >> /root/cpulimit-log.log
+
+  fi
+
+  sleep 3
+
+done
